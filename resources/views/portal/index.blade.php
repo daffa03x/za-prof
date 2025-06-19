1eddbd05690ad780624730dbc2b31442a4298dc4
daffa03x <dafaputra876@gmail.com>
2025-06-19 08:45:40 +0700

    ubah testimoni

--- resources/views/portal/index.blade.php
+++ resources/views/portal/index.blade.php
@@ -549,15 +549,13 @@
                                         class="bi bi-star-fill"></i>
                                 </div>
                                 <p>
-                                    Proin iaculis purus consequat sem cure digni ssim donec porttitora entum suscipit
-                                    rhoncus. Accusantium quam, ultricies eget id, aliquam eget nibh et. Maecen aliquam,
-                                    risus at semper.
+                                    Seru banget ketemu temen baru dan adik-adik yang lucu. Kegiatannya keren!
                                 </p>
                                 <div class="profile mt-auto">
                                     <img src="assets/img/testimonials/testimonials-1.jpg" class="testimonial-img"
                                         alt="">
-                                    <h3>Saul Goodman</h3>
-                                    <h4>Ceo &amp; Founder</h4>
+                                    <h3>Nayla Nur Adzima</h3>
+                                    <h4>Volunteer</h4>
                                 </div>
                             </div>
                         </div><!-- End testimonial item -->
@@ -570,15 +568,14 @@
                                         class="bi bi-star-fill"></i>
                                 </div>
                                 <p>
-                                    Export tempor illum tamen malis malis eram quae irure esse labore quem cillum quid
-                                    cillum eram malis quorum velit fore eram velit sunt aliqua noster fugiat irure amet
-                                    legam anim culpa.
+                                    Ketemu temen-temen baru dengan background bermacam-macam. tapi punya hobi yang sama.
+                                    Trims Zillenial Action!
                                 </p>
                                 <div class="profile mt-auto">
                                     <img src="assets/img/testimonials/testimonials-2.jpg" class="testimonial-img"
                                         alt="">
-                                    <h3>Sara Wilsson</h3>
-                                    <h4>Designer</h4>
+                                    <h3>Elsa Arinda</h3>
+                                    <h4>Volunteer</h4>
                                 </div>
                             </div>
                         </div><!-- End testimonial item -->
@@ -591,14 +588,14 @@
                                         class="bi bi-star-fill"></i>
                                 </div>
                                 <p>
-                                    Enim nisi quem export duis labore cillum quae magna enim sint quorum nulla quem veniam
-                                    duis minim tempor labore quem eram duis noster aute amet eram fore quis sint minim.
+                                    Kesekian kalinya ikut jadi volunteer bareng Zillenial Action. Seru, kegiatannya beraneka
+                                    ragam banget!!
                                 </p>
                                 <div class="profile mt-auto">
                                     <img src="assets/img/testimonials/testimonials-3.jpg" class="testimonial-img"
                                         alt="">
-                                    <h3>Jena Karlis</h3>
-                                    <h4>Store Owner</h4>
+                                    <h3>Agniya Aulia</h3>
+                                    <h4>Volunteer</h4>
                                 </div>
                             </div>
                         </div><!-- End testimonial item -->
@@ -611,15 +608,14 @@
                                         class="bi bi-star-fill"></i>
                                 </div>
                                 <p>
-                                    Fugiat enim eram quae cillum dolore dolor amet nulla culpa multos export minim fugiat
-                                    minim velit minim dolor enim duis veniam ipsum anim magna sunt elit fore quem dolore
-                                    labore illum veniam.
+                                    Ketemu banyak orang hebat yang punya semangat berbagi. Seneng plus hati puas banget!
+                                    Rekomended banget buat kalian yang mau isi waktu dengan hal positif!
                                 </p>
                                 <div class="profile mt-auto">
                                     <img src="assets/img/testimonials/testimonials-4.jpg" class="testimonial-img"
                                         alt="">
-                                    <h3>Matt Brandon</h3>
-                                    <h4>Freelancer</h4>
+                                    <h3>Nabila Virla A.</h3>
+                                    <h4>Volunteer</h4>
                                 </div>
                             </div>
                         </div><!-- End testimonial item -->
@@ -632,15 +628,14 @@
                                         class="bi bi-star-fill"></i>
                                 </div>
                                 <p>
-                                    Quis quorum aliqua sint quem legam fore sunt eram irure aliqua veniam tempor noster
-                                    veniam enim culpa labore duis sunt culpa nulla illum cillum fugiat legam esse veniam
-                                    culpa fore nisi cillum quid.
+                                    Dapet Pengalaman baru, temen baru, dan adik adik baru juga! Ga pernah nyesel ikut
+                                    volunteer bareng ZA!
                                 </p>
                                 <div class="profile mt-auto">
                                     <img src="assets/img/testimonials/testimonials-5.jpg" class="testimonial-img"
                                         alt="">
-                                    <h3>John Larson</h3>
-                                    <h4>Entrepreneur</h4>
+                                    <h3>Aldira Aprilia</h3>
+                                    <h4>Volunteer</h4>
                                 </div>
                             </div>
                         </div><!-- End testimonial item -->
