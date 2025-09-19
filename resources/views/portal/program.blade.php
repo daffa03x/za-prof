729ac1d2e1c9027a75f9c21bbd4793894e8f0586
daffa03x <dafaputra876@gmail.com>
2025-09-19 15:51:04 +0700

    masukan id pixel meta

--- resources/views/portal/program.blade.php
+++ resources/views/portal/program.blade.php
@@ -23,12 +23,12 @@
             s.parentNode.insertBefore(t, s)
         }(window, document, 'script',
             'https://connect.facebook.net/en_US/fbevents.js');
-        fbq('init', '{your-pixel-id-goes-here}');
+        fbq('init', '824012039961095');
         fbq('track', 'PageView');
     </script>
     <noscript>
         <img height="1" width="1" style="display:none"
-            src="https://www.facebook.com/tr?id={your-pixel-id-goes-here}&ev=PageView&noscript=1" />
+            src="https://www.facebook.com/tr?id=824012039961095&ev=PageView&noscript=1" />
     </noscript>
     <!-- End Facebook Pixel Code -->
 
