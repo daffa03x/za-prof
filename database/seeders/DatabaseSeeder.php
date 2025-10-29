b2d0c8e049ad285efb229c3c3e3ff70b562c5797
daffa03x <dafaputra876@gmail.com>
2025-10-29 10:07:31 +0700

    matiin seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,22 +15,22 @@ class DatabaseSeeder extends Seeder
         \App\Models\User::factory()->admin()->create();
         
         // Membuat 2 user biasa
-        \App\Models\User::factory(2)->create();
+        // \App\Models\User::factory(2)->create();
         
         // Membuat 1 campaign
-        \App\Models\Campaign::factory(1)->create();
+        // \App\Models\Campaign::factory(1)->create();
         
         // Membuat 6 event
-        \App\Models\Event::factory(6)->create();
+        // \App\Models\Event::factory(6)->create();
 
         // Membuat 5 payment
-        \App\Models\Payment::factory(5)->create();
+        // \App\Models\Payment::factory(5)->create();
 
         // Membuat 10 transaksi
-        \App\Models\Transaksi::factory(10)->create();
+        // \App\Models\Transaksi::factory(10)->create();
 
         // Membuat 10 pixel
-        \App\Models\Pixel::factory(4)->create();
+        // \App\Models\Pixel::factory(4)->create();
 
         
 
