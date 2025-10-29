d81319d819a0acd1ea3bfcb6a95af037e0b78720
daffa03x <dafaputra876@gmail.com>
2025-10-29 10:16:54 +0700

    tambah no rek

--- database/migrations/2024_10_29_023122_create_payments_table.php
+++ database/migrations/2024_10_29_023122_create_payments_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('image')->nullable();
+            $table->string('no_rek');
             $table->boolean('status')->default(0);
             $table->timestamps();
             $table->softDeletes();
